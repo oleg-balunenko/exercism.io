db58d7de13066d545cb5f259094df857fab9aa67
Oleg Balunenko <oleg.balunenko@be-mobile.com>
2017-12-17 20:53:51 +0300

    Bob: Implement some logic and add stubs

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -6,31 +6,56 @@
 package bob
 
 import (
-	"strings"
+	"bytes"
+	"fmt"
 )
-
-
 
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
 
-	strings.ContainsAny(remark, "?")
+	fmt.Println("The last char is: ", string(remark[len(remark)-1]))
+	arr := make([]byte, len(remark))
+	copy(arr[:], remark)
+
+	if isQuestion(remark) {
+		arr = bytes.Trim(arr, "?")
+
+		if isUppercase(remark) {
+			return "Calm down, I know what I'm doing!"
+		}
+		return "Sure"
+
+	}
+
+	if isUppercase(remark) {
+		return "Whoa, chill out!"
+	} else {
+
+	}
 
 	switch remark {
 
-
-
-	case "question?":
-		return "Sure"
-	case "Yell (uppercase)":
-		return "Whoa, chill out!"
-	case "yell question? (uppercase with?)":
-		return "Calm down, I know what I'm doing!"
 	case "":
 		return "Fine. Be that way!"
 	default:
 		return "Whatever."
 
+	}
+}
+
+func isQuestion(remark string) (question bool) {
+
+	lastSymbol := string(remark[len(remark)-1])
+	if lastSymbol == "?" {
+
+		return true
 
 	}
+	return false
+
+}
+
+func isUppercase(remark string) (allUppercase bool) {
+
+	return true
 }
